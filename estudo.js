bfe044a99623888a00dda491a26e3d0aa73d15aa
Alexandre <103534039+AlexSakai@users.noreply.github.com>
2022-09-28 18:17:36 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -2,6 +2,18 @@
 //console.log ("Oi, eu estou no console.");
 //document.write("Aula de Pens. Comp.")
 
+
+let tabuada = 2;
+
 function meChame() {
-    alert("Oi, eu sou um alert!");
+    document.write (tabuada + " x 1 = " + (tabuada * 1) + "<br>");
+    document.write (tabuada + " x 1 = " + (tabuada * 2) + "<br>");
+    document.write (tabuada + " x 1 = " + (tabuada * 3) + "<br>");
+    document.write (tabuada + " x 1 = " + (tabuada * 4) + "<br>");
+    document.write (tabuada + " x 1 = " + (tabuada * 5) + "<br>");
+    document.write (tabuada + " x 1 = " + (tabuada * 6) + "<br>");
+    document.write (tabuada + " x 1 = " + (tabuada * 7) + "<br>");
+    document.write (tabuada + " x 1 = " + (tabuada * 8) + "<br>");
+    document.write (tabuada + " x 1 = " + (tabuada * 9) + "<br>");
+    document.write (tabuada + " x 1 = " + (tabuada * 10) + "<br>");
 }

c60f88ce516baf6eeb67bd79689f47d5c0d4bac2
Alexandre <103534039+AlexSakai@users.noreply.github.com>
2022-10-19 18:15:02 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -37,15 +37,33 @@ function total(){
     let juros = document.getElementById("ju").value;
     let mes = document.getElementById("mes").value;
 
+    if(!Number(valor)){
+        alert("O o valor deve ser um número.");
+
+        document.getElementById("val").value ="";
+        document.getElementById("val").focus();
+        return
+    }
+    if(!Number(juros)){
+        alert("Os juros deve ser um número.");
+
+        document.getElementById("ju").value ="";
+        document.getElementById("ju").focus();
+        return
+    }
+    if(!Number(mes)){
+        alert("Os meses devem ser um número.");
+
+        document.getElementById("mes").value ="";
+        document.getElementById("mes").focus();
+        return
+    }
+
     let resultado = 0;
     for(let i = 1; i <= mes; i++){
         resultado = valor * (1+(juros/100));
         valor = resultado;
-    }
-    
-    if(!Number(valor)){
-        alert("O valor deve ser um número.");
-        return
+        
     }
 
     document.write("O Resultado é " + resultado);
